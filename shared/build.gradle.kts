16e746f4971bd5d990ea31676a5b54444c08056d
Jaikel19 <jaikel.guzman@outlook.com>
2026-02-19 21:24:33 -0600

    paso2
    completo

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -80,9 +80,28 @@ kotlin {
 
         androidMain {
             dependencies {
-                // Add Android-specific dependencies here. Note that this source set depends on
-                // commonMain by default and will correctly pull the Android artifacts of any KMP
-                // dependencies declared in commonMain.
+                implementation(libs.ktor.client.okhttp)
+            }
+            commonMain.dependencies {
+                // ... other dependencies
+                // Coroutine
+                implementation(libs.kotlinx.coroutines.core)
+
+                // DateTime
+                implementation(libs.kotlinx.datetime)
+
+                // Ktor
+                implementation(project.dependencies.platform(libs.ktor.bom))
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.serialization.kotlinx.json)
+            }
+            commonTest.dependencies {
+                // ... other dependencies
+                // Coroutine
+                implementation(libs.kotlinx.coroutines.test)
+                // Ktor
+                implementation(libs.ktor.client.mock)
             }
         }
 
@@ -96,11 +115,7 @@ kotlin {
 
         iosMain {
             dependencies {
-                // Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
-                // Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
-                // part of KMP’s default source set hierarchy. Note that this source set depends
-                // on common by default and will correctly pull the iOS artifacts of any
-                // KMP dependencies declared in commonMain.
+                implementation(libs.ktor.client.darwin)
             }
         }
     }
